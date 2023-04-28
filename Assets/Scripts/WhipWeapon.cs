50c77fda1483356ae99cc911088f06ad2ef435eb
Nikita <nikitabuldakov@mail.ru>
2023-04-28 14:12:18 +0300

    Add comments to WhipWeapon

--- Assets/Scripts/WhipWeapon.cs
+++ Assets/Scripts/WhipWeapon.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Класс оружия-хлыста
+/// </summary>
 public class Whip : MonoBehaviour
 {
     [SerializeField] float timeToAttack = 4f;
@@ -14,22 +18,31 @@ public class Whip : MonoBehaviour
     [SerializeField] Vector2 whipAttackSize = new Vector2(4f, 2f);
     [SerializeField] int whipDamage = 1;
 
+    /// <summary>
+    /// Метод исполняется при загрузке сцены
+    /// </summary>
     private void Awake(){
         playerMove = GetComponentInParent<PlayerMove>();
     }
 
     private void Update()
-    {
+    {   
+        // Отсчет таймера
         timer -= Time.deltaTime;
+        // Когда таймер доходит до нуля, хлыст атакует
         if (timer < 0f){
             Attack();
         }
     }
 
+    /// <summary>
+    /// Атака оружия
+    /// </summary>
     private void Attack(){
         //Debug.Log("Whip Attack");
+        // Возобновление таймера
         timer = timeToAttack;
-
+        // Расчет анимации атаки
         if(playerMove.lastHorizontalVector > 0){
             rightWhipObject.SetActive(true);
             Collider2D[] colliders = Physics2D.OverlapBoxAll(rightWhipObject.transform.position, whipAttackSize, 0f);
@@ -41,11 +54,17 @@ public class Whip : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Нанесение урона
+    /// </summary>
+    /// <param name="colliders">Коллизия объекта с хлыстом</param>
     private void ApplyDamage(Collider2D[] colliders){
         for(int i = 0; i<colliders.Length; ++i){
             //Debug.Log(colliders[i].gameObject.name);
+            // Получение объекта коллизии
             IDamagable e = colliders[i].GetComponent<IDamagable>();
             if (e != null){
+                // Нанесение урона
                 e.TakeDamage(whipDamage);
             }
         }
